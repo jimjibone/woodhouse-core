314589b481999ad6232e2f04119f5360883f90ab
jimjibone <jamesreuss@gmail.com>
2022-11-07 22:25:35 +0000

    basic device state and value display in webapp

---
+++ webapp/src/components/DeviceValue.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { DeviceValue } from '../api/device_pb';
+
+	export let value: DeviceValue = null;
+</script>
+
+{#if value.hasBool()}
+<p>{value.getName()} Bool: {value.getBool() ? "true" : "false"}</p>
+{:else if value.hasNumber()}
+<p>{value.getName()} Number: {value.getNumber()}</p>
+{:else if value.hasText()}
+<p>{value.getName()} Text: {value.getText()}</p>
+{/if}
--- webapp/src/pages/DevicesPage.svelte
+++ webapp/src/pages/DevicesPage.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte/internal';
-    import type { Unsubscriber } from 'svelte/store';
-	import type { DeviceInfo } from '../api/device_pb';
-	import { deviceInfosStream } from '../stores/devices';
+	import type { Unsubscriber } from 'svelte/store';
+	import type { DeviceInfo, DeviceState } from '../api/device_pb';
+    import DeviceValue from '../components/DeviceValue.svelte';
+	import { deviceInfosStream, deviceStatesStream } from '../stores/devices';
 
 	let deviceInfos: DeviceInfo[] = [];
 	let deviceInfosConnected: boolean = false;
+	let deviceStates: DeviceState[] = [];
+	let deviceStatesConnected: boolean = false;
 	let unsubscribeDeviceInfos: Unsubscriber = null;
 	let unsubscribeDeviceInfosConnected: Unsubscriber = null;
+	let unsubscribeDeviceStates: Unsubscriber = null;
+	let unsubscribeDeviceStatesConnected: Unsubscriber = null;
 
 	onMount(async () => {
 		console.log("DevicesPage onMount");
 		unsubscribeDeviceInfos = deviceInfosStream.subscribeData(value => { deviceInfos = value; });
 		unsubscribeDeviceInfosConnected = deviceInfosStream.subscribeConnected(value => { deviceInfosConnected = value; });
+		unsubscribeDeviceStates = deviceStatesStream.subscribeData(value => { deviceStates = value; });
+		unsubscribeDeviceStatesConnected = deviceStatesStream.subscribeConnected(value => { deviceStatesConnected = value; });
 	});
 
 	onDestroy(() => {
 		console.log("DevicesPage onDestroy");
 		unsubscribeDeviceInfos();
 		unsubscribeDeviceInfosConnected();
+		unsubscribeDeviceStates();
+		unsubscribeDeviceStatesConnected();
 	});
 </script>
 
@@ -36,10 +45,30 @@
 		</p>
 	</div>
 </section>
+
+<h2>Infos</h2>
 {#each deviceInfos as info (info.getDeviceId())}
 <div class="column is-full">
 	<div class="box">
 		{info.getBridgeId()} {info.getDeviceId()} {info.getName()} <a href="{info.getUrl()}">{info.getUrl()}</a>
+	</div>
+</div>
+{:else}
+<div class="column is-full">
+	<div class="box">
+		No devices.
+	</div>
+</div>
+{/each}
+
+<h2>States</h2>
+{#each deviceStates as state (state.getDeviceId())}
+<div class="column is-full">
+	<div class="box">
+		{state.getBridgeId()} {state.getDeviceId()} {state.getValuesList().length}
+		{#each state.getValuesList() as value (value.getName())}
+			<DeviceValue value={value} />
+		{/each}
 	</div>
 </div>
 {:else}
