ebd51c5cbc34148d9e1c9c34124596580a5714bf
jimjibone <jamesreuss@gmail.com>
2024-04-24 22:50:17 +0100

    get sveltekit to build a single page app

--- webapp2/svelte.config.js
+++ webapp2/svelte.config.js
@@ -1,13 +1,14 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		// Use the static adapter along with 'ssr = false' in +layout.js to
+		// generate a single page app.
+		adapter: adapter({
+			fallback: "index.html"
+		}),
 
 		alias: {
 			"@/*": "./src/lib/*",
---
+++ webapp2/webapp.go
@@ -0,0 +1,6 @@
+package webapp
+
+import "embed"
+
+//go:embed build/*
+var Content embed.FS
