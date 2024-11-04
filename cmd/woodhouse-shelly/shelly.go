1bb6ae7bed4a5f38113ff3283bb217312aa7feb2
jimjibone <jamesreuss@gmail.com>
2024-11-04 21:32:56 +0000

    potential fix for reconnection issue with shelly devices

--- cmd/woodhouse-shelly/shelly.go
+++ cmd/woodhouse-shelly/shelly.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/grandcat/zeroconf"
 	"github.com/jimjibone/woodhouse-4/cmd/woodhouse-shelly/shelly_v1"
@@ -45,10 +46,47 @@ func shellyStuff(wg *sync.WaitGroup, ctx context.Context, client *wh.Client) err
 		}
 	}()
 
+	discover := func(ip, hostname string) (retry bool) {
+		exists := false
+		if _, found := devices_v1[hostname]; found {
+			exists = true
+		}
+		if _, found := devices_v2[hostname]; found {
+			exists = true
+		}
+
+		if !exists {
+			_, device_v1, device_v2, retry := handleDiscovery(ip, hostname, client)
+			if retry {
+				return true
+			}
+
+			if device_v1 != nil {
+				devices_v1[device_v1.ID()] = device_v1
+			}
+			if device_v2 != nil {
+				devices_v2[device_v2.ID()] = device_v2
+			}
+		}
+
+		return false
+	}
+
+	retryList := make(map[string]string) // key=hostname, value=ip
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
+
+		case <-ticker.C:
+			for hostname, ip := range retryList {
+				log.Infof("retrying connection to %s at %s", hostname, ip)
+				if !discover(ip, hostname) {
+					delete(retryList, hostname)
+				}
+			}
 
 		case entry := <-entries:
 			if entry != nil {
@@ -58,41 +96,28 @@ func shellyStuff(wg *sync.WaitGroup, ctx context.Context, client *wh.Client) err
 				}
 				hostname := strings.TrimSuffix(entry.HostName, ".local.")
 
-				exists := false
-				if _, found := devices_v1[hostname]; found {
-					exists = true
-				}
-				if _, found := devices_v2[hostname]; found {
-					exists = true
-				}
-
-				if !exists {
-					_, device_v1, device_v2 := handleDiscovery(ip, hostname, client)
-					if device_v1 != nil {
-						devices_v1[device_v1.ID()] = device_v1
-					}
-					if device_v2 != nil {
-						devices_v2[device_v2.ID()] = device_v2
-					}
+				log.Infof("trying connection to %s at %s", hostname, ip)
+				if discover(ip, hostname) {
+					retryList[hostname] = ip
 				}
 			}
 		}
 	}
 }
 
-func handleDiscovery(ip, hostname string, client *wh.Client) (deviceID string, device_v1 shelly_v1.Device, device_v2 shelly_v2.Device) {
+func handleDiscovery(ip, hostname string, client *wh.Client) (deviceID string, device_v1 shelly_v1.Device, device_v2 shelly_v2.Device, retry bool) {
 	// Gen 1 Device API - https://shelly-api-docs.shelly.cloud/gen1
 	if strings.Contains(hostname, "shelly") {
 		rest := shelly_v1.NewRest(ip)
 		settings, err := rest.GetSettings()
 		if err != nil {
 			log.Errorf("failed to get settings for %s: %s", ip, err)
-			return "", nil, nil
+			return "", nil, nil, true
 		}
 
 		log.Infof("discovered v1 %s at http://%s (name: %s, type: %s)", hostname, ip, settings.Name, settings.Device.Type)
 
-		return hostname, shelly_v1.Generate(settings.Device.Type, hostname, ip, client), nil
+		return hostname, shelly_v1.Generate(settings.Device.Type, hostname, ip, client), nil, false
 	}
 
 	// Gen 2 Device API - https://shelly-api-docs.shelly.cloud/gen2
@@ -101,15 +126,15 @@ func handleDiscovery(ip, hostname string, client *wh.Client) (deviceID string, d
 		info, err := rest.GetShelly()
 		if err != nil {
 			log.Errorf("failed to get shelly info for %s: %s", ip, err)
-			return "", nil, nil
+			return "", nil, nil, true
 		}
 
 		log.Infof("discovered v2 %s at http://%s (name: %s, app: %s)", hostname, ip, info.Name, info.App)
 
-		return hostname, nil, shelly_v2.Generate(info.App, hostname, ip, info.Name, client)
+		return hostname, nil, shelly_v2.Generate(info.App, hostname, ip, info.Name, client), false
 	}
 
 	log.Warnf("other thing %s at http://%s", hostname, ip)
 
-	return "", nil, nil
+	return "", nil, nil, false
 }
