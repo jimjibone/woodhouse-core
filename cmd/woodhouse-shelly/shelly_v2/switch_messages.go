e0e4fcbe5a1b0ed42802a246d2c70a52491f8611
jimjibone <jamesreuss@gmail.com>
2026-06-19 20:11:28 +0100

    removed bridge implementations after moving to dedicated repos

--- cmd/woodhouse-shelly/shelly_v2/switch_messages.go
+++
@@ -1,7 +0,0 @@
-package shelly_v2
-
-type SwitchSet struct {
-	ID          int      `json:"id"`           // ID of the Switch component instance. Required.
-	On          bool     `json:"on"`           // True for switch on, false otherwise. Required.
-	ToggleAfter *float64 `json:"toggle_after"` // Optional flip-back timer in seconds. Optional.
-}
